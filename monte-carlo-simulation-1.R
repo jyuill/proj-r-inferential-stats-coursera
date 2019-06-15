799ddc6be529bd00d62d068183e423116239c238
jyuill <john@fig4.com>
2019-06-15 12:33:58 -0700

    working with distributions

--- monte-carlo-simulation-1.R
+++ monte-carlo-simulation-1.R
@@ -7,18 +7,26 @@
 
 library(tidyverse)
 library(gridExtra)
+
+par(mfrow=c(2,2)) ## setup for rows, columns of charts
+
+## generate sample of session data, based on normal distribution
 sess <- rnorm(100, mean=100, sd=12)
 sess
 hist(sess)
 
+## generate sample of conversion rate data, based on normal distribution 
 cr <- rnorm(100, mean=0.1, sd=0.02)
 cr
 hist(cr)
-
+## generate sample of ave. spend data, based on normal distribution
 avesp <- rnorm(100, mean=2.50, sd=0.55)
 avesp
 hist(avesp)
 
+## generate sample of rev, based on calculations from the sample data for sess, cr, avesp
+## rev data has normal distribution because based on data with normal distributions
+## only problem is not realistic: at least some of the data will have a skew
 rev <- sess * cr * avesp
 rev
 hist(rev)
@@ -26,20 +34,37 @@ mean(rev)
 sd(rev)
 
 ### generating skewed distribution
+par(mfrow=c(1,1)) ## reset chart layout
 ## basic example log normal distribution
 ldist <- rlnorm(100, meanlog=0, sdlog=1)
 hist(ldist)
 
 ## avesp
+par(mfrow=c(2,1)) ## may need to expand Plots pane
+## skewed distrib for ave spend
 avesp.lg <- rlnorm(100, meanlog=2.50, sdlog=0.55)
-par(mfrow=c(1,2))
-hist(avesp.lg)
-hist(avesp)
+hist(avesp.lg) 
+hist(avesp) ## compare to ave spend distribution from above (normal)
+
+## same as above using ggplot2
 df.avesp <- data.frame(avesp.lg=avesp.lg,
                        avesp=avesp)
 p.avesp <- ggplot(df.avesp, aes(x=avesp))+geom_histogram()+
   geom_vline(xintercept = mean(avesp))
 p.avesp.lg <- ggplot(df.avesp, aes(x=avesp.lg))+geom_histogram()+
   geom_vline(xintercept=mean(avesp.lg))
-
+## use grid arrange to display charts together
 grid.arrange(p.avesp, p.avesp.lg, nrow=2)
+
+## looking at different distributions
+pois1 <- rpois(n=100, lambda = 3)
+par(mfrow=c(1,2))
+plot(pois1)
+hist(pois1)
+
+## QUESTIONS:
+## 1. how to transform log dist into original units?
+
+df.avesp <- df.avesp %>% mutate(
+  avesp.trans = e^avesp.lg
+)
